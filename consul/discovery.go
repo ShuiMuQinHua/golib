51bbcbbb98a51488c66488a75e45ae5bb994cfbd
caiyunyun <caiyunyun@xunlei.com>
2021-09-03 17:41:29 +0800

    代码调整

--- consul/discovery.go
+++ consul/discovery.go
@@ -1,93 +1,92 @@
 package consul
 
 import (
-  "fmt"
-  "sync"
-  "sync/atomic"
+	"fmt"
+	"sync"
+	"sync/atomic"
 )
 
 type Discovery interface {
-  GetInstance(serviceName string) (string, error)
+	GetInstance(serviceName string) (string, error)
 }
 
 type discovery struct {
-  srcRegistry ServiceRegistry
-  service2Instances *sync.Map
-  service2Index *sync.Map
+	srcRegistry       ServiceRegistry
+	service2Instances *sync.Map
+	service2Index     *sync.Map
 }
 
 func NewDiscovery(consulAddr string, token string) (Discovery, error) {
-  //服务注册
-  registryDiscoveryClient, err := NewConsulServiceRegistry(consulAddr, token)
-  if err != nil {
-    return nil, err
-  }
+	//服务注册
+	registryDiscoveryClient, err := NewConsulServiceRegistry(consulAddr, token)
+	if err != nil {
+		return nil, err
+	}
 
-  reg := &discovery{
-    srcRegistry: registryDiscoveryClient,
-    service2Instances: &sync.Map{},
-    service2Index: &sync.Map{},
-  }
+	reg := &discovery{
+		srcRegistry:       registryDiscoveryClient,
+		service2Instances: &sync.Map{},
+		service2Index:     &sync.Map{},
+	}
 
-  return reg, nil
+	return reg, nil
 }
 
 func (r *discovery) GetInstance(serviceName string) (string, error) {
-  var serviceList []ServiceInstance
-  var instance string
-  var err error
-  if val, ok := r.service2Instances.Load(serviceName); ok {
-    serviceList = val.([]ServiceInstance)
-  } else {
-    serviceList, err = r.watchAndSave(serviceName)
-    if err != nil {
-      return "", err
-    }
-  }
+	var serviceList []ServiceInstance
+	var instance string
+	var err error
+	if val, ok := r.service2Instances.Load(serviceName); ok {
+		serviceList = val.([]ServiceInstance)
+	} else {
+		serviceList, err = r.watchAndSave(serviceName)
+		if err != nil {
+			return "", err
+		}
+	}
 
-  if len(serviceList) == 0 {
-    return "", fmt.Errorf("not found serviceList")
-  }
+	if len(serviceList) == 0 {
+		return "", fmt.Errorf("not found serviceList")
+	}
 
-  if data, ok := r.service2Index.Load(serviceName); ok {
-    if indexAddr, ok := data.(*uint64); ok {
-      index := atomic.AddUint64(indexAddr, 1)
-      if index > 1000000000 {
-        atomic.StoreUint64(indexAddr, 0)
-        index = 0
-      }
-      rIndex := index % uint64(len(serviceList))
-      sInstance := serviceList[rIndex]
-      instance = getUrlFromInstance(sInstance)
-    }
-  }
+	if data, ok := r.service2Index.Load(serviceName); ok {
+		if indexAddr, ok := data.(*uint64); ok {
+			index := atomic.AddUint64(indexAddr, 1)
+			if index > 1000000000 {
+				atomic.StoreUint64(indexAddr, 0)
+				index = 0
+			}
+			rIndex := index % uint64(len(serviceList))
+			sInstance := serviceList[rIndex]
+			instance = getUrlFromInstance(sInstance)
+		}
+	}
 
-  return instance, nil
+	return instance, nil
 }
 
 func (r *discovery) watchAndSave(serviceName string) ([]ServiceInstance, error) {
-  handler := func(svcName string, instances []ServiceInstance) {
-    r.service2Instances.Store(svcName, instances)
-  }
-  
-  serviceList, err := r.srcRegistry.GetInstances(serviceName)
-  if err != nil {
-    return nil, err
-  }
+	handler := func(svcName string, instances []ServiceInstance) {
+		r.service2Instances.Store(svcName, instances)
+	}
 
-  var index uint64
-  r.service2Instances.Store(serviceName, serviceList)
-  r.service2Index.Store(serviceName, &index)
-  go r.srcRegistry.WatchPlan(serviceName, handler)
-  return serviceList, err
+	serviceList, err := r.srcRegistry.GetInstances(serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	var index uint64
+	r.service2Instances.Store(serviceName, serviceList)
+	r.service2Index.Store(serviceName, &index)
+	go r.srcRegistry.WatchPlan(serviceName, handler)
+	return serviceList, err
 }
 
 func getUrlFromInstance(instance ServiceInstance) string {
-  schema := "http"
-  if instance.IsSecure() {
-    schema = "https"
-  }
+	schema := "http"
+	if instance.IsSecure() {
+		schema = "https"
+	}
 
-  return fmt.Sprintf("%s://%s:%d", schema, instance.GetHost(), instance.GetPort())
+	return fmt.Sprintf("%s://%s:%d", schema, instance.GetHost(), instance.GetPort())
 }
-
--- consul/service_discovery.go
+++ consul/service_discovery.go
@@ -16,8 +16,8 @@ type ServiceRegistry interface {
 }
 
 type consulServiceRegistry struct {
-	addr string
-	token string
+	addr                 string
+	token                string
 	serviceInstances     map[string]map[string]ServiceInstance
 	client               api.Client
 	localServiceInstance ServiceInstance
@@ -44,28 +44,28 @@ func NewConsulServiceRegistry(addr string, token string) (ServiceRegistry, error
 func (c *consulServiceRegistry) WatchPlan(serviceName string, handler func(string, []ServiceInstance)) error {
 	watchConfig := make(map[string]interface{})
 
-  watchConfig["type"] = "service"
-  watchConfig["service"] = serviceName
-  watchConfig["handler_type"] = "script"
-  watchConfig["passingonly"] = true
+	watchConfig["type"] = "service"
+	watchConfig["service"] = serviceName
+	watchConfig["handler_type"] = "script"
+	watchConfig["passingonly"] = true
 
-  watchPlan, err := watch.Parse(watchConfig)
-  if err != nil {
-  	return err
-  }
+	watchPlan, err := watch.Parse(watchConfig)
+	if err != nil {
+		return err
+	}
 
-  cb := func(lastIndex uint64, result interface{}) {
-      serviceList := result.([]*api.ServiceEntry)
-			ret := make([]ServiceInstance, len(serviceList))
-			for index, service := range serviceList {
-				ret[index] = getServiceInstance(service)
-			}
-			handler(serviceName, ret)
-  }
+	cb := func(lastIndex uint64, result interface{}) {
+		serviceList := result.([]*api.ServiceEntry)
+		ret := make([]ServiceInstance, len(serviceList))
+		for index, service := range serviceList {
+			ret[index] = getServiceInstance(service)
+		}
+		handler(serviceName, ret)
+	}
 
-  watchPlan.Handler = cb
+	watchPlan.Handler = cb
 
-  return watchPlan.Run(c.addr)
+	return watchPlan.Run(c.addr)
 }
 
 /**
@@ -187,4 +187,3 @@ func (c *consulServiceRegistry) GetServices() ([]string, error) {
 	}
 	return result, nil
 }
-
